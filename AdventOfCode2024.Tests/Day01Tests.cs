e54136fac41c289e3ed72c763b2050a78f2f381e
Brian Guerrero <13bguerrero@gmail.com>
2024-12-11 16:40:49 -0600

    Solutin for Day 1 of Advent Of Code

---
+++ AdventOfCode2024.Tests/Day01Tests.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day01Tests
+{
+    private readonly static List<int> Locations1 = new List<int> { 3, 4, 2, 1, 3, 3 };
+    private readonly static List<int> Locations2 = new List<int> { 4,3,5,3,9,3 };
+
+    [Fact]
+    public void PairDistancesAreCorrect()
+    {
+        var pairs = Day01.GetPairs(Locations1, Locations2).ToList();
+        var expected = new List<int> { 2, 1, 0, 1, 2, 5 };
+        Assert.Equal(expected, pairs.Select(p => p.Distance));
+    }
+
+    [Fact]
+    public void CalculateDistanceIsCorrect()
+    {
+        var distance = Day01.CalculateDistance(Locations1, Locations2);
+        Assert.Equal(11, distance);
+    }
+    
+    [Fact]
+    public void GetSimilarityScoreIsCorrect()
+    {
+        var score = Day01.GetSimilarityScore(Locations2, 3);
+        Assert.Equal(9, score);
+    }
+
+    [Fact]
+    public void CalculateSimilarityScoreIsCorrect()
+    {
+        var similarityScore = Day01.CalculateSimilarityScore(Locations1, Locations2);
+        Assert.Equal(31, similarityScore);
+    }
+}
+
+public class PairTests
+{
+    [Fact]
+    public void DistanceIsCorrect()
+    {
+        var pair = new Pair(1, 3);
+        Assert.Equal(2, pair.Distance);
+    }
+}
---
+++ AdventOfCode2024/Day01.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2024;
+
+public class Day01
+{
+
+    public static int GetSimilarityScore(List<int> locations, int locationId)
+    {
+        return locationId * locations.Count(x => x == locationId);
+    }
+
+    public static IEnumerable<Pair> GetPairs(List<int> locations1, List<int> locations2)
+    {
+        for (var i = 0; i < locations1.Count; i++)
+        {
+            yield return new Pair(locations1.Order().ToList()[i], locations2.Order().ToList()[i]);
+        }
+    }
+
+    public static int CalculateDistance(List<int> locations1, List<int> locations2)
+    {
+        return GetPairs(locations1, locations2).Sum(p => p.Distance);
+    }
+    
+    public static int CalculateSimilarityScore(List<int> locations1, List<int> locations2)
+    {
+        var sum = 0;
+        foreach (var (locationId, count) in locations1.CountBy(x => x))
+        {
+            sum += count * GetSimilarityScore(locations2, locationId);
+        }
+
+        return sum;
+    }
+}
+
+public record Pair(int X, int Y)
+{
+    public int Distance => Math.Abs(X - Y);
+}
