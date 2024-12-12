acdeefcf27084def3cfcd46aeaf48c27184468a9
Brian Guerrero <13bguerrero@gmail.com>
2024-12-12 00:08:42 -0600

    Day 4 Part 1

---
+++ AdventOfCode2024.Tests/Day04Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day04Tests
+{
+    [Fact]
+    public void CountXmasesIsCorrect()
+    {
+        const string input = """
+                             ....XXMAS.
+                             .SAMXMS...
+                             ...S..A...
+                             ..A.A.MS.X
+                             XMASAMX.MM
+                             X.....XA.A
+                             S.S.S.S.SS
+                             .A.A.A.A.A
+                             ..M.M.M.MM
+                             .X.X.XMASX
+                             """;
+        var count = Day04.CountXMASes(input);
+        Assert.Equal(18, count);
+    }
+}
---
+++ AdventOfCode2024/Day04.cs
@@ -0,0 +1,132 @@
+﻿namespace AdventOfCode2024;
+
+public class Day04
+{
+    public static CharacterMap ParseMap(string input)
+    {
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        var map = new char[lines.Length, lines[0].Length];
+        for (var y = 0; y < lines.Length; y++)
+        {
+            for (var x = 0; x < lines[y].Length; x++)
+            {
+                map[y, x] = lines[y][x];
+            }
+        }
+
+        return new CharacterMap(map);
+    }
+
+    public static int CountXMASes(string input)
+    {
+        var map = ParseMap(input);
+        var count = 0;
+        for (var y = 0; y < map.Height; y++)
+        {
+            for (var x = 0; x < map.Width; x++)
+            {
+                if (map[x, y] == 'X')
+                {
+                    if (map.IsInBounds(x + 1, y) && map[x + 1, y] == 'M')
+                    {
+                        if (map.IsInBounds(x + 2, y) && map[x + 2, y] == 'A')
+                        {
+                            if (map.IsInBounds(x + 3, y) && map[x + 3, y] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    if (map.IsInBounds(x - 1, y) && map[x - 1, y] == 'M')
+                    {
+                        if (map.IsInBounds(x - 2, y) && map[x - 2, y] == 'A')
+                        {
+                            if (map.IsInBounds(x - 3, y) && map[x - 3, y] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    if (map.IsInBounds(x, y + 1) && map[x, y + 1] == 'M')
+                    {
+                        if (map.IsInBounds(x, y + 2) && map[x, y + 2] == 'A')
+                        {
+                            if (map.IsInBounds(x, y + 3) && map[x, y + 3] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    if (map.IsInBounds(x, y - 1) && map[x, y - 1] == 'M')
+                    {
+                        if (map.IsInBounds(x, y - 2) && map[x, y - 2] == 'A')
+                        {
+                            if (map.IsInBounds(x, y - 3) && map[x, y - 3] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    if (map.IsInBounds(x + 1, y + 1) && map[x + 1, y + 1] == 'M')
+                    {
+                        if (map.IsInBounds(x + 2, y + 2) && map[x + 2, y + 2] == 'A')
+                        {
+                            if (map.IsInBounds(x + 3, y + 3) && map[x + 3, y + 3] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    if (map.IsInBounds(x - 1, y - 1) && map[x - 1, y - 1] == 'M')
+                    {
+                        if (map.IsInBounds(x - 2, y - 2) && map[x - 2, y - 2] == 'A')
+                        {
+                            if (map.IsInBounds(x - 3, y - 3) && map[x - 3, y - 3] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                    if (map.IsInBounds(x - 1, y + 1) && map[x - 1, y + 1] == 'M')
+                    {
+                        if (map.IsInBounds(x - 2, y + 2) && map[x - 2, y + 2] == 'A')
+                        {
+                            if (map.IsInBounds(x - 3, y + 3) && map[x - 3, y + 3] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    if (map.IsInBounds(x + 1, y - 1) && map[x + 1, y - 1] == 'M')
+                    {
+                        if (map.IsInBounds(x + 2, y - 2) && map[x + 2, y - 2] == 'A')
+                        {
+                            if (map.IsInBounds(x + 3, y - 3) && map[x + 3, y - 3] == 'S')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+    
+}
+
+public record CharacterMap(char[,] Map)
+{
+    public int Width => Map.GetLength(1);
+    public int Height => Map.GetLength(0);
+    public char this[int x, int y] => Map[y, x];
+
+    public bool IsInBounds(int x, int y) => x >= 0 && x < Width && y >= 0 && y < Height;
+}
