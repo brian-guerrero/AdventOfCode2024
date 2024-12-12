357a3a6df68684c7e6b2e12162913adcc10a848a
Brian Guerrero <13bguerrero@gmail.com>
2024-12-12 12:23:28 -0600

    Day 5 Part 1

---
+++ AdventOfCode2024.Tests/Day05Tests.cs
@@ -0,0 +1,78 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day05Tests
+{
+    [Fact]
+    public void CountPrintableUpdatesIsCorrect()
+    {
+        const string input = """
+                             47|53
+                             97|13
+                             97|61
+                             97|47
+                             75|29
+                             61|13
+                             75|53
+                             29|13
+                             97|29
+                             53|29
+                             61|53
+                             97|53
+                             61|29
+                             47|13
+                             75|47
+                             97|75
+                             47|61
+                             75|61
+                             47|29
+                             75|13
+                             53|13
+
+                             75,47,61,53,29
+                             97,61,53,29,13
+                             75,29,13
+                             75,97,47,61,53
+                             61,13,29
+                             97,13,75,29,47
+                             """;
+        var result = Day05.CountPrintableUpdates(input);
+        Assert.Equal(3, result);
+    }
+    
+    [Fact]
+    public void MiddlePageSumOfPrintableUpdatesIsCorrect()
+    {
+        const string input = """
+                             47|53
+                             97|13
+                             97|61
+                             97|47
+                             75|29
+                             61|13
+                             75|53
+                             29|13
+                             97|29
+                             53|29
+                             61|53
+                             97|53
+                             61|29
+                             47|13
+                             75|47
+                             97|75
+                             47|61
+                             75|61
+                             47|29
+                             75|13
+                             53|13
+
+                             75,47,61,53,29
+                             97,61,53,29,13
+                             75,29,13
+                             75,97,47,61,53
+                             61,13,29
+                             97,13,75,29,47
+                             """;
+        var result = Day05.MiddlePageSumOfPrintableUpdates(input);
+        Assert.Equal(143, result);
+    }
+}
---
+++ AdventOfCode2024/Day05.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace AdventOfCode2024;
+
+public class Day05
+{
+    public static PrintQueue ParsePrintQueue(string printQueueInstructions)
+    {
+        var sections = printQueueInstructions.Split("\r\n\r\n");
+
+        var rules = sections[0].Split("\r\n")
+            .Select(rule => rule.Split("|"))
+            .Select(rule => new PageOrderingRule(int.Parse(rule[0]), int.Parse(rule[1])));
+
+        var updates = sections[1].Split("\r\n")
+            .Select(update => update.Split(","))
+            .Select(update => new PageUpdates(update.Select(int.Parse).ToList()));
+
+        return new PrintQueue(rules, updates);
+    }
+
+    public static int CountPrintableUpdates(string printQueueInstructions)
+    {
+        var queue = ParsePrintQueue(printQueueInstructions);
+        return queue.PrintableUpdates().Count();
+    }
+    
+    public static int MiddlePageSumOfPrintableUpdates(string printQueueInstructions)
+    {
+        var queue = ParsePrintQueue(printQueueInstructions);
+        return queue.PrintableUpdates().Sum(update => update.MiddlePage);
+    }
+}
+
+public record PrintQueue(IEnumerable<PageOrderingRule> Rules, IEnumerable<PageUpdates> Updates)
+{
+    public IEnumerable<PageUpdates> PrintableUpdates()
+    {
+        foreach(var update in Updates)
+        {
+            var applicableRules = Rules.Where(rule => update.PageNumbers.Contains(rule.PrecedingPage) && update.PageNumbers.Contains(rule.FollowingPage));
+            if(applicableRules.All(rule => update.FollowRule(rule)))
+            {
+                yield return update;
+            }
+        }
+    }
+};
+
+public record PageOrderingRule(int PrecedingPage, int FollowingPage);
+
+
+public record PageUpdates(List<int> PageNumbers)
+{
+    public bool FollowRule(PageOrderingRule rule)
+    {
+        return PageNumbers.IndexOf(rule.PrecedingPage) < PageNumbers.IndexOf(rule.FollowingPage);
+    }
+
+    public int MiddlePage => PageNumbers.Skip(PageNumbers.Count/2).First();
+};
+
