34a0a688f518dfba73e970fdd6fa471ff1869d42
Brian Guerrero <13bguerrero@gmail.com>
2024-12-11 19:31:53 -0600

    Day 3 Solutions

---
+++ AdventOfCode2024.Tests/Day03Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day03Tests
+{
+    [Theory]
+    [InlineData("mul(44,46)", 2024)]
+    [InlineData("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", 161)]
+    public void MulInstructionsAreParsedCorrectly(string input, int expected)
+    {
+        var results = Day03.EvaluateMulInstructionsFromCorruptedString(input);
+        Assert.Equal(expected, results);
+    }
+    
+    [Theory]
+    [InlineData("mul(44,46)", 2024)]
+    [InlineData("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", 48)]
+    public void MulInstructionsWithDoOrDontAreParsedCorrectly(string input, int expected)
+    {
+        var results = Day03.EvaluateMulInstructionsWithDoOrDontFromCorruptedString(input);
+        Assert.Equal(expected, results);
+    }
+}
---
+++ AdventOfCode2024/Day03.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024;
+
+public partial class Day03
+{
+    
+    public static IEnumerable<MulInstruction> ParseMulInstructions(string input)
+    {
+        var instructions = MulRegex().Matches(input);
+        foreach (Match instruction in instructions)
+        {
+            var a = int.Parse(instruction.Groups[1].Value);
+            var b = int.Parse(instruction.Groups[2].Value);
+            yield return new MulInstruction(a, b);
+        }
+    }
+    
+    public static IEnumerable<MulInstruction> ParseMulInstructionsWithDoOrDontOperands(string input)
+    {
+        var instructions = MulWithDoOrDontRegex().Matches(input);
+        var skipMul = false;
+        foreach (Match instruction in instructions)
+        {
+            if (instruction.Groups[1].Value == "do()")
+            {
+                skipMul = false;
+                continue;
+            }
+            if (instruction.Groups[1].Value == "don't()")
+            {
+                skipMul = true;
+                continue;
+            }
+
+            if (skipMul) continue;
+            var a = int.Parse(instruction.Groups[2].Value);
+            var b = int.Parse(instruction.Groups[3].Value);
+            yield return new MulInstruction(a, b);
+        }
+    }
+
+    public static int EvaluateMulInstructionsFromCorruptedString(string input)
+    {
+        return ParseMulInstructions(input).Sum(instruction => instruction.Result);
+    }
+    
+    public static int EvaluateMulInstructionsWithDoOrDontFromCorruptedString(string input)
+    {
+        return ParseMulInstructionsWithDoOrDontOperands(input).Sum(instruction => instruction.Result);
+    }
+    
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+    private static partial Regex MulRegex();
+    
+    [GeneratedRegex(@"(do\(\)|don't\(\)|mul\((\d+),(\d+)\))")]
+    private static partial Regex MulWithDoOrDontRegex();
+}
+
+public record MulInstruction(int A, int B)
+{
+    public int Result => A * B;
+}
