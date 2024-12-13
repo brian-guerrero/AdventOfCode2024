0f7702877f8324db87927618509926ec707faf72
Brian Guerrero <13bguerrero@gmail.com>
2024-12-13 12:52:06 -0600

    Day 7 Solution

---
+++ AdventOfCode2024.Tests/Day07Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2024.Tests;
+
+public class Day07Tests
+{
+    [Fact]
+    public void CalculateTotalCalibrationResultIsCorrect()
+    {
+        const string input = """
+                             190: 10 19
+                             3267: 81 40 27
+                             83: 17 5
+                             156: 15 6
+                             7290: 6 8 6 15
+                             161011: 16 10 13
+                             192: 17 8 14
+                             21037: 9 7 18 13
+                             292: 11 6 16 20
+                             """;
+        var result=  Day07.CalculateTotalCalibrationResult(input);
+        Assert.Equal(3749, result);
+    }
+}
---
+++ AdventOfCode2024/Day07.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+
+namespace AdventOfCode2024;
+
+public class Day07
+{
+    private static IEnumerable<CalibrationEquation> ParseCalibationEquation(string input)
+    {
+        var lines = input.Split("\r\n");
+        foreach (var line in lines)
+        {
+            var equation = line.Split(":");
+            var testValue = long.Parse(equation[0]);
+            var equationValues = equation[1].Trim().Split(" ").Select(long.Parse).ToList();
+            yield return new CalibrationEquation(testValue, equationValues);
+        }
+    }
+
+    public static long CalculateTotalCalibrationResult(string input)
+    {
+        var equations = ParseCalibationEquation(input);
+        var correctlyCalibratedFunctions = equations
+            .Where(e => GenerateCalibrationFunctions(e)
+                .Any(f => f() == e.TestValue));
+        return correctlyCalibratedFunctions.Sum(e => e.TestValue);
+    }
+
+    private static IEnumerable<Func<long>> GenerateCalibrationFunctions(CalibrationEquation equation)
+    {
+        var operationCombinations = GetPossibleOperatorCombinations(equation.EquationValues.Count - 1);
+
+        return operationCombinations.Select(opCombo => GenerateCalibrationFunctions(equation, opCombo));
+    }
+
+    private static Func<long> GenerateCalibrationFunctions(CalibrationEquation equation, char[] opCombo)
+    {
+        Expression expression = Expression.Constant(equation.EquationValues[0]);
+        var i = 1;
+        expression = opCombo.Aggregate(expression, (current, op) => op switch
+        {
+            '+' => Expression.Add(current, Expression.Constant(equation.EquationValues[i++])),
+            '*' => Expression.Multiply(current, Expression.Constant(equation.EquationValues[i++])),
+            _ => throw new ArgumentOutOfRangeException()
+        });
+
+        return Expression.Lambda<Func<long>>(expression).Compile();
+    }
+
+    private static IEnumerable<char[]> GetPossibleOperatorCombinations(int length)
+    {
+        for (var i = 0; i < Math.Pow(CalibrationOperators.Length, length); i++)
+        {
+            var binary = Convert.ToString(i, 2).PadLeft(length, '0');
+            yield return binary.Select(c => c == '0' ? '+' : '*').ToArray();
+        }
+    }
+
+
+    private static readonly char[] CalibrationOperators = ['+', '*'];
+}
+
+public record CalibrationEquation(long TestValue, List<long> EquationValues);
